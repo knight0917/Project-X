db7cf2e2b8b102f23affe5f22ed57795f38c34e4
knightmoon <ankitakshay78@gmail.com>
2024-06-03 17:36:11 +0530

    removed single digit from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import MainForm from "@/components/MainForm";
-// import Elements from "@/components/gridplane/Elements";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <MainForm />
-      {/* <Elements singleDigits={[2]}/> */}
     </div>
   );
 }
