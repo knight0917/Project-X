11d4cf90f3181c16a053edef1812b2bfa4446bc3
Caelum Pirata <caelumpirata@gmail.com>
2024-06-12 15:48:26 +0530

    some more ui changes

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
 import { Inter } from "next/font/google";
 import Link from "next/link";
-
+import MainForm from "./main-form";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen text-center ">
+
+    <p className="my-6"> this site is all about knowing youself.</p>
+
       <nav>
         <ul>
           <li>
-            <Link href="/main-form">
-               Know your numerology
+            <Link className="underline hover:bg-green-900" href="/main-form">
+               Know your NUMEROLOGY
             </Link>
           </li>
           <li>
-            <Link href="/match-form">
-              Match your partner
+            <Link  className="underline hover:bg-green-900" href="/match-form">
+              Match your NUMEROLOGY with your partner
             </Link>
           </li>
         </ul>
